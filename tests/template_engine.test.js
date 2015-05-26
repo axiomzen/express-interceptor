9df7f3f5f8cab863776c9468038d8c7cb0dbcb0f
Gustav Nikolaj Olsen <gno@one.com>
2015-05-26 23:11:00 +0200

    rewrite tests/template_engine.test.js

--- tests/template_engine.test.js
+++ tests/template_engine.test.js
@@ -1,19 +1,19 @@
-var request = require('supertest');
-var expect  = require('chai').expect;
-var fs      = require('fs');
-
 var app = require('../examples/template_engine');
+var expect = require('unexpected')
+    .clone()
+    .installPlugin(require('unexpected-express'))
+    .addAssertion('to yield response', function (expect, subject, value) {
+        return expect(app, 'to yield exchange', {
+            request: subject,
+            response: value
+        });
+    });
 
 describe('Using a template engine (Jade)', function() {
 
-  it('should intercept the generated .html and respond with the same .html', function(done) {
-    request(app)
-      .get('/index')
-      .expect(200)
-      .end(function(err, res) {
-        var hack = '<script>alert("you were intercepted!")</script>';
-        expect(res.text).to.contain(hack);
-        done(err);
+  it('should intercept the generated .html and respond with the same .html', function() {
+      return expect('/index', 'to yield response', {
+          body: expect.it('to contain', '<script>alert("you were intercepted!")</script>')
       });
   });
 
